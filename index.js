df67673c6ae6d8da941d593961d94cb62fa07d22
Tamojit Das <tamojitdas181007@gmail.com>
2024-06-29 17:44:50 +0530

    mongoDB init

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 dotenv.config();
 const app = express();
+
+const mongoURI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`; //mongodb://username:password@mongoIP_or_auto reference to mongoImg:default_port/?authSource=admin
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((e) => {
+        console.log(e);
+    })
 
 const PORT = process.env.PORT || 3000;
 
