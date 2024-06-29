a5538e7da55746b252f699e5bf748ea906ce3013
Tamojit Das <tamojitdas181007@gmail.com>
2024-06-29 19:01:32 +0530

    crud init

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
+const postRouter = require("./routes/postRoutes");
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+
 const mongoURI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`; //mongodb://username:password@mongoIP_or_auto reference to mongoImg:default_port/?authSource=admin
 
 const connectWithRetry = () => {
     mongoose.connect(mongoURI)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((e) => {
-        console.log(e);
-        setTimeout(connectWithRetry, 5000); //If not connected --> wait for 5secs --> then again try to connect
-    })
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+        .catch((e) => {
+            console.log(e);
+            setTimeout(connectWithRetry, 5000); //If not connected --> wait for 5secs --> then again try to connect
+        })
 }
 connectWithRetry();
 
@@ -23,7 +26,8 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello from Docker-Node.js!!</h2>");
-})
+});
+app.use("/api/v1/posts", postRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`);
